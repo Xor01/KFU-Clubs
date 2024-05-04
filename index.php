86383b4e4afccd90663379d0649b9500c3725a67
az1z0z <lionps55@gmail.com>
2024-05-04 21:02:04 +0300

    add index,login,main Css file

---
+++ index.php
@@ -0,0 +1,100 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="mainpage.css"
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>webproject</title>
+</head>
+<body>
+    <div id="navbar">
+
+<h3 id="Login"><a href="login.php" style="text-decoration: none; color: black;">Login/Register</a></h3>
+<div class="dropdown">
+
+<h3 id="Courses">Courses</h3>
+<div class="dropdown-content">
+
+<a>Cybersecurity</a>
+<a>Google</a>
+<a>AIP</a>
+
+</div>
+</div>
+<h3 id="announcments"><a style="text-decoration: none; color: black;">announcments</a></h3>
+
+<h3 id="Events">Events</h3>
+
+
+
+
+
+    </div>
+<div id="Paragraphs">
+    <h2 class="CCSIT KFU">CCSIT KFU</h2>
+
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat voluptatem repellendus adipisci consequatur id aperiam ducimus cumque officia asperiores tempore alias aut, voluptate a, atque soluta. Architecto rerum reiciendis commodi?</p>
+
+<h2 class="Aboutus">About us</h2>
+<p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga amet nostrum a, corrupti, vel pariatur odit error quia sequi nulla cum itaque libero corporis consectetur fugit reprehenderit, similique neque iste.</p>
+
+
+</div>
+
+<div id="Background">
+
+    <div id="Clubsinfo">
+        <h3>Clubs </h3>
+        <div id="Container">
+            <div class="club-container">
+                <div class="club-info">
+                    <h4>Cybersecurity Club</h4>
+                    <p>&#128373;</p> 
+                </div>
+            </div>
+            <div class="club-container">
+                <div class="club-info">
+                    <h4>Google Club</h4>
+                    <p>&#128736;</p> 
+                </div>
+            </div>
+            <div class="club-container">
+                <div class="club-info">
+                    <h4>AIP Club</h4>
+                    <p>&#128214;</p> 
+                </div>
+            </div>
+        </div>
+    </div>
+    
+</div>
+
+
+<div id="clubcontents">
+    <h3 style="font-size: larger;">Club outcomes</h3>
+    <div id="clubsoutcomes">
+        <div class="club">
+            <h4 style="font-size: large;">Cybersecurity Club</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+        <div class="club">
+            <h4 style="font-size: large;">Google Club</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+        <div class="club">
+            <h4 style="font-size: large;">AIP Club</h4>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+        </div>
+    </div>
+</div>
+
+
+
+
+
+
+
+
+    
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Login</title>
+    <link rel="stylesheet" href="Loginpage.css">
+</head>
+<body>
+    <div id="box1">
+<h1>Login</h1>
+
+<div id="box2">
+<form>
+<label for="Email">Email:</label>
+<input type="email" name="Email" id="Email" placeholder="Email"><br><br><br>
+
+<label for="Pass">Password:</label>
+<input type="password" name="Pass" id="Pass" placeholder="Password"><br><br>
+
+<button id="LoginBtn">Login</button>
+</form>
+
+</div>
+
+
+    </div>
+</body>
+</html>
