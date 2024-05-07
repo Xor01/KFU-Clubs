9aa02a8d98c0cb72406b478aa75fe374bb994d79
Xor01 <111096981+Xor01@users.noreply.github.com>
2024-05-07 23:59:38 +0300

    change login and create register

--- register.php
+++ register.php
@@ -7,23 +7,22 @@ require_once "nav.php";
 <div class="container-sm">
     <div class="d-flex justify-content-center  align-items-center py-4">
         
-        <main class="form-login w-50 m-auto">
+        <main class="form-register w-50 m-auto">
             <form action="" method="POST">
-                <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+                <h1 class="h3 mb-3 fw-normal">Welcome To KFU Club, Please Create your account!</h1>
             
                 <div class="form-floating">
                 <input type="email" class="form-control" id="email" name="email" placeholder="name@example.com" required>
                 <label for="email">Email address</label>
                 </div>
-                <br>
                 <div class="form-floating">
                 <input type="password" class="form-control" id="password" name="password" placeholder="Password" required>
                 <label for="password">Password</label>
                 </div>
                 <br>
-                <button class="btn btn-primary w-100 py-2" type="submit">Sign in</button>
+                <button class="btn btn-primary w-100 py-2" type="submit">Register</button>
                 <hr>
-                <a href="register.php"><input type="button" action="register.php" class="btn btn-secondary w-100 py-2" value="Don't Have account ? Register instead"></input></a>
+                <a href="login.php"><input type="button" action="register.php" class="btn btn-secondary w-100 py-2" value="Have account ? login instead"></input></a>
             </form>
         </main>
     </div>
