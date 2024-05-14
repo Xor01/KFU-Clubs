0e3fa19bbdd99822e294059f721133cc3cc19939
Xor01 <111096981+Xor01@users.noreply.github.com>
2024-05-15 02:25:22 +0300

    add latest database dump

--- current_sql_script.sql
+++ current_sql_script.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: May 14, 2024 at 01:14 AM
+-- Generation Time: May 15, 2024 at 01:24 AM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -24,61 +24,61 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `clubmembers`
+-- Table structure for table `events`
 --
 
-CREATE TABLE `clubmembers` (
-  `clubMemberID` int(11) NOT NULL,
-  `clubID` int(11) NOT NULL,
-  `userID` int(11) NOT NULL,
-  `roleID` int(11) NOT NULL,
-  `active` tinyint(1) NOT NULL DEFAULT 0
+CREATE TABLE `events` (
+  `eventID` int(11) NOT NULL,
+  `clubID` int(11) DEFAULT NULL,
+  `eventTitle` varchar(255) NOT NULL,
+  `description` text NOT NULL,
+  `start_datetime` datetime NOT NULL,
+  `end_datetime` datetime NOT NULL,
+  `location` varchar(255) NOT NULL,
+  `created_by` int(11) NOT NULL,
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data for table `clubmembers`
+-- Dumping data for table `events`
 --
 
-INSERT INTO `clubmembers` (`clubMemberID`, `clubID`, `userID`, `roleID`, `active`) VALUES
-(1, 1, 21, 1, 1),
-(2, 2, 21, 1, 1),
-(4, 1, 22, 2, 0),
-(5, 2, 22, 2, 0);
+INSERT INTO `events` (`eventID`, `clubID`, `eventTitle`, `description`, `start_datetime`, `end_datetime`, `location`, `created_by`, `created_at`) VALUES
+(1, 1, 'AI Bootcamp', 'There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don\'t look even slightly believable. If you are going to use a passage of Lorem Ipsum, you need to be sure there isn\'t anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet. It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.\n\n', '2024-05-15 19:31:48', '2024-05-16 19:31:48', 'CCSIT building 15', 21, '2024-05-14 16:32:48'),
+(5, 2, 'Cyber Security Fundamentals', 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.\n\n', '2024-05-14 21:41:00', '2024-05-15 21:42:00', 'CCSIT Main building', 21, '2024-05-14 18:43:15');
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indexes for table `clubmembers`
+-- Indexes for table `events`
 --
-ALTER TABLE `clubmembers`
-  ADD PRIMARY KEY (`clubMemberID`),
+ALTER TABLE `events`
+  ADD PRIMARY KEY (`eventID`),
   ADD KEY `clubID` (`clubID`),
-  ADD KEY `userID` (`userID`),
-  ADD KEY `roleID` (`roleID`);
+  ADD KEY `created_by` (`created_by`);
 
 --
 -- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT for table `clubmembers`
+-- AUTO_INCREMENT for table `events`
 --
-ALTER TABLE `clubmembers`
-  MODIFY `clubMemberID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+ALTER TABLE `events`
+  MODIFY `eventID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
 
 --
 -- Constraints for dumped tables
 --
 
 --
--- Constraints for table `clubmembers`
+-- Constraints for table `events`
 --
-ALTER TABLE `clubmembers`
-  ADD CONSTRAINT `clubmembers_ibfk_1` FOREIGN KEY (`clubID`) REFERENCES `clubs` (`clubID`),
-  ADD CONSTRAINT `clubmembers_ibfk_2` FOREIGN KEY (`userID`) REFERENCES `users` (`uid`),
-  ADD CONSTRAINT `clubmembers_ibfk_3` FOREIGN KEY (`roleID`) REFERENCES `roles` (`roleID`);
+ALTER TABLE `events`
+  ADD CONSTRAINT `events_ibfk_1` FOREIGN KEY (`clubID`) REFERENCES `clubs` (`clubID`),
+  ADD CONSTRAINT `events_ibfk_2` FOREIGN KEY (`created_by`) REFERENCES `users` (`uid`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
