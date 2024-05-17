0245c5d2fbf69c2a0ab3016c61f05057a060127e
Xor01 <111096981+Xor01@users.noreply.github.com>
2024-05-18 01:31:05 +0300

    users now can register to events

--- app/backend/auth/register_for_event.php
+++ app/backend/auth/register_for_event.php
@@ -2,11 +2,41 @@
 
 require_once "app/backend/core/Init.php";
 
+
 if (!$user->isLoggedIn()){
     Session::flash('general', 'To Register for the this event you need to log in.');
     Redirect::to('login.php');
 }
 
-if (Input::get('userId') && Input::get('eventId')) {
-    echo "hello world";
+
+$event_id = Input::get('event_id');
+$user_id = Input::get('user_id');
+$club_id = Input::get('club_id');
+
+if (Input::get('action')) {
+    if ($events->find($event_id)){
+        
+        if (!$clubManagement->isUserInClub($clubId, $userId)){
+            Session::flash('general', 'Only Members can register in this event.');
+            Redirect::to('view_event.php');
+        }
+        
+        $result = $events->getRegistrationStatus($user_id, $event_id);
+        
+        if ($result) {
+            Session::flash('general', 'You have already registered to attend this event, Your attendance status is: ');
+            Redirect::to("view_event.php?userId=$user_id&event_id=$event_id");
+        }
+
+        $result = $events->sendRegistration($club_id, $user_id, $event_id);
+        if ($result) {
+            Session::flash('general', 'Thank you have successfully registered to this event, please check later to see if you have been accepted.');
+            Redirect::to('view_event.php?userId=$user_id&event_id=$event_id');
+        }
+    }
+    else{
+        Session::flash('general', 'Sorry We could not find this event information.');
+        Redirect::to('events.php');
+    }
+    
 }
--- view_event.php
+++ view_event.php
@@ -1,5 +1,7 @@
 <?php require_once 'start.php'; ?>
 <?php require_once FRONTEND_INCLUDE . 'header.php'; ?>
 <?php require_once FRONTEND_INCLUDE . 'navbar.php'; ?>
+<?php require_once FRONTEND_INCLUDE . 'messages.php'; ?>
+<?php require_once BACKEND_AUTH . 'register_for_event.php'; ?>
 <?php require_once FRONTEND_PAGE . 'view_event.php'; ?>
 <?php require_once FRONTEND_INCLUDE . 'footer.php'; ?>
