f840d6b6b8a5da807e661f5a427a9a529d5677f5
Xor01 <111096981+Xor01@users.noreply.github.com>
2024-05-21 00:15:24 +0300

    change editing privileges to club admin instead of system admin, fix

--- app/backend/auth/change_privileges.php
+++ app/backend/auth/change_privileges.php
@@ -1,15 +1,16 @@
 <?php
 
-if (!$user->hasPermission('admin')) {
-    Session::flash('danger', 'You do not have permission to access this page');
-    Redirect::to('applicant_management.php');
-}
-
-
 $roleID = escape(Input::get('roleID'));
 $userID = escape(Input::get('userID'));
 $clubID = escape(Input::get('clubID'));
+
 if ($roleID) {
+
+    if (!$clubManagement->isUserIsAnAdminToThisClub($user->data()->uid, $clubID)) {
+        Session::flash('danger', 'You do not have permission to access this page');
+        Redirect::to('applicant_management.php');
+    }
+    
    $result =  $clubManagement->changePermission($roleID, $userID, $clubID);
    if ($result) {
     Session::flash('general', 'Successfully change permission.');
