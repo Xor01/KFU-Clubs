f92840e5bbde75941c409a6491a544ab663ed74a
Xor01 <111096981+Xor01@users.noreply.github.com>
2024-05-20 19:43:59 +0300

    remove home style from header file and import it in the home.php file

--- app/frontend/pages/home.php
+++ app/frontend/pages/home.php
@@ -1,45 +1,6 @@
-<!-- <div id="Paragraphs" class="container">
-    <div class="row">
-        <div class="col-md-6">
-            <h2 class="CCSIT KFU">CCSIT KFU</h2>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat voluptatem repellendus adipisci consequatur id aperiam ducimus cumque officia asperiores tempore alias aut, voluptate a, atque soluta. Architecto rerum reiciendis commodi?
-            </p>
-        </div>
-        <div class="col-md-6">
-            <h2 class="Aboutus">About us</h2>
-            <p>
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga amet nostrum a, corrupti, vel pariatur odit error quia sequi nulla cum itaque libero corporis consectetur fugit reprehenderit, similique neque iste.
-            </p>
-        </div>
-    </div>
-</div> -->
-
-<!-- <div class="container">
-<div class="row">
-      <div class="col-lg-4">
-        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg>
-        <h2 class="fw-normal">CCSIT KFU</h2>
-        <p>Some representative placeholder content for the three columns of text below the carousel. This is the first column.</p>
-        <p><a class="btn btn-secondary" href="#">View details »</a></p>
-      </div>
-      <div class="col-lg-4">
-        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg>
-        <h2 class="fw-normal">About us
-</h2>
-        <p>Another exciting bit of representative placeholder content. This time, we've moved on to the second column.</p>
-        <p><a class="btn btn-secondary" href="#">View details »</a></p>
-      </div>
-      <div class="col-lg-4">
-        <svg class="bd-placeholder-img rounded-circle" width="140" height="140" xmlns="http://www.w3.org/2000/svg" role="img" aria-label="Placeholder" preserveAspectRatio="xMidYMid slice" focusable="false"><title>Placeholder</title><rect width="100%" height="100%" fill="var(--bs-secondary-color)"></rect></svg>
-        <h2 class="fw-normal">Heading</h2>
-        <p>And lastly this, the third column of representative placeholder content.</p>
-        <p><a class="btn btn-secondary" href="#">View details »</a></p>
-      </div>
-    </div>
-</div> -->
-
-
+<head>
+    <link rel="stylesheet" href="<link href="<?php echo FRONTEND_ASSET . 'css/profile.css'; ?>">
+</head>
 <div class="row g-4 py-5 row-cols-1 row-cols-lg-3 justify-content-center ">
       <div class="feature col">
         <div class="feature-icon d-inline-flex align-items-center justify-content-center text-bg-primary bg-gradient fs-2 mb-3">
